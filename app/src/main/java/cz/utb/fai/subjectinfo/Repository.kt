91c1c3c0f42ab24040484c80d68ca52e851f56f4
Radek Vala <radek.www@gmail.com>
2023-10-21 15:17:04 +0200

    Repository - apiService injected into constructor getSubjectInfo method added, return is SubjectInfo or null

--- app/src/main/java/cz/utb/fai/subjectinfo/Repository.kt
+++ app/src/main/java/cz/utb/fai/subjectinfo/Repository.kt
@@ -1,4 +1,17 @@
 package cz.utb.fai.subjectinfo
 
-class Repository (private val apiService: ...) {
+import cz.utb.fai.subjectinfo.api.StagApiService
+import cz.utb.fai.subjectinfo.model.SubjectInfo
+
+class Repository (private val apiService: StagApiService) {
+
+    suspend fun getSubjectInfo (katedra: String, zkratka: String) : SubjectInfo? {
+        val response = apiService.getSubjectInfo(katedra, zkratka)
+
+        if(response.isSuccessful) {
+            return response.body()
+        } else {
+            return null
+        }
+    }
 }
