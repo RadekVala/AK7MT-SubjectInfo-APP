152dfeddc1c22a4a364bc68741377fbdcf5c8dfe
Radek Vala <radek.www@gmail.com>
2023-10-21 16:08:20 +0200

    SubjectInfoViewModel - provides subjectInfoValue property (SubjectInfo) - provides getSubjectInfo()

--- app/src/main/java/cz/utb/fai/subjectinfo/SubjectInfoViewModel.kt
+++ app/src/main/java/cz/utb/fai/subjectinfo/SubjectInfoViewModel.kt
@@ -1,10 +1,24 @@
 package cz.utb.fai.subjectinfo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cz.utb.fai.subjectinfo.model.SubjectInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class SubjectInfoViewModel(
     private val repository: Repository
 ) : ViewModel() {
 
+    private val _subjectInfoValue = MutableLiveData<SubjectInfo>()
+    val subjectInfoValue: LiveData<SubjectInfo> = _subjectInfoValue
+    fun getSubjectInfo(katedra: String, zkratka: String){
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = repository.getSubjectInfo(katedra, zkratka)
+            _subjectInfoValue.postValue(result)
+        }
+    }
 }
