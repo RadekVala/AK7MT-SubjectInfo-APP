cc7c24d7f4145a51306c7ffd64ba9bfc9afb4590
Radek Vala <radek.www@gmail.com>
2023-10-21 15:14:57 +0200

    StagApiService - method getSubjectInfo (HTTP request against REST API) added, return type is Response<SubjectInfo>

--- app/src/main/java/cz/utb/fai/subjectinfo/api/StagApiService.kt
+++ app/src/main/java/cz/utb/fai/subjectinfo/api/StagApiService.kt
@@ -1,6 +1,20 @@
 package cz.utb.fai.subjectinfo.api
 
-class StagApiService {
+import cz.utb.fai.subjectinfo.model.SubjectInfo
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface StagApiService {
 
+    companion object {
+        const val SUBJECT_INFO_ENDPOINT = "predmety/getPredmetInfo"
+    }
+
+    @GET(SUBJECT_INFO_ENDPOINT)
+    suspend fun getSubjectInfo (
+        @Query("katedra") katedra: String,
+        @Query("zkratka") zkratka: String,
+        @Query("outputFormat") outputFormat: String = "json"
+    ) : Response<SubjectInfo>
 }
