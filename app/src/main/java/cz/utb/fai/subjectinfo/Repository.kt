2e1d34de0ade567f40f312f6c3e8ade15e445d22
Radek Vala <radek.www@gmail.com>
2023-11-24 21:24:43 +0100

    Repository - changed, two methods are there: getSubjectInfo is method providing data to ViewModel from database and triggering refreshing data stored in database from Rest API Method refreshSubjects gets data from API and stores into DB

--- app/src/main/java/cz/utb/fai/subjectinfo/Repository.kt
+++ app/src/main/java/cz/utb/fai/subjectinfo/Repository.kt
@@ -1,17 +1,51 @@
 package cz.utb.fai.subjectinfo
 
+import android.util.Log
 import cz.utb.fai.subjectinfo.api.StagApiService
+import cz.utb.fai.subjectinfo.database.SubjectEntity
+import cz.utb.fai.subjectinfo.database.SubjectInfoDatabase
 import cz.utb.fai.subjectinfo.model.SubjectInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
-class Repository (private val apiService: StagApiService) {
+class Repository (private val apiService: StagApiService, private val database: SubjectInfoDatabase) {
 
-    suspend fun getSubjectInfo (katedra: String, zkratka: String) : SubjectInfo? {
-        val response = apiService.getSubjectInfo(katedra, zkratka)
 
-        if(response.isSuccessful) {
-            return response.body()
-        } else {
-            return null
+    // Fetch subject from Room database
+    suspend fun getSubjectInfo(katedra: String, zkratka: String): Flow<SubjectInfo> {
+        refreshSubjects(katedra, zkratka)
+        return database.subjectInfoDao.getSubjectByZkratka(zkratka)
+            .map { it.asDomainModel() }
+    }
+
+    // Refresh data from API and save to Room database
+    suspend fun refreshSubjects(katedra: String, zkratka: String) {
+        try {
+            val apiResponse = apiService.getSubjectInfo(katedra, zkratka)
+            val subjectInfo: SubjectInfo? = apiResponse.body()
+
+            if(subjectInfo != null) {
+                // convert domain model from REST API to database entity
+                val subjectEntity = SubjectEntity(
+                    0,
+                    subjectInfo.nazev,
+                    subjectInfo.zkratka,
+                    subjectInfo.kreditu,
+                    subjectInfo.typZkousky
+                    )
+
+                // Save to Room database
+                withContext(Dispatchers.IO) {
+                    database.subjectInfoDao.insert(subjectEntity)
+                }
+            }
+
+        } catch (e: Exception) {
+            // Handle API call errors
+            Log.e("SubjectRepository", "Error refreshing subjects" + e.localizedMessage)
         }
     }
+
 }
